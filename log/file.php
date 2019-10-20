9fc623a4edcf68c954584e271f23299bcce192de
yaoyang <kiki@smarty.so>
2019-10-20 17:40:13 +0800

    调整日志时间展示逻辑

--- log/file.php
+++ log/file.php
@@ -1,11 +1,16 @@
 <?php
 
+function _log_prefix()
+{/*{{{*/
+    return '['.date('Y-m-d H:i:s '.substr((string) microtime(), 2, 8)).']';
+}/*}}}*/
+
 function log_exception(throwable $ex)
 {/*{{{*/
 
     $log = config('log');
 
-    error_log('['.date('Y-m-d H:i:s u').']'.$ex."\n", 3, $log['exception_path']);
+    error_log(_log_prefix().$ex."\n", 3, $log['exception_path']);
 }/*}}}*/
 
 function log_notice($message)
@@ -13,12 +18,12 @@ function log_notice($message)
 
     $log = config('log');
 
-    error_log('['.date('Y-m-d H:i:s u').']'.$message."\n", 3, $log['notice_path']);
+    error_log(_log_prefix().$message."\n", 3, $log['notice_path']);
 }/*}}}*/
 
 function log_module($module, $message)
 {/*{{{*/
     $log = config('log');
 
-    error_log('['.date('Y-m-d H:i:s u').']'.'['.$module.'] '.$message."\n", 3, $log['module_path']);
+    error_log(_log_prefix().'['.$module.'] '.$message."\n", 3, $log['module_path']);
 }/*}}}*/
