0f267854631c6942c20d0de1d3ece7f5e3d0f2a6
yaoyang <kiki@smarty.so>
2019-11-18 14:16:30 +0800

    添加在工作单元执行过程中声明工作单元成功或者失败后的任务委托

--- unitofwork.php
+++ unitofwork.php
@@ -20,52 +20,103 @@ function _unit_of_work_write($sql_template, array $binds = [], $config_key = 'de
     otherwise($row_count === 1, 'data in unit of work is expired');
 }
 
+function if_unit_of_work_executed($action = null)
+{
+    static $container = null;
+
+    if (is_array($action)) {
+        return $container = null;
+    } else if (! empty($action)) {
+        return $container = $action;
+    } 
+
+    return $container;
+}
+
+function if_unit_of_work_disturbed($action = null, $ex = null)
+{
+    static $container = null;
+
+    if (is_array($action)) {
+        return $container = null;
+    } else if (! empty($action)) {
+        return $container = $action;
+    } 
+
+    return $container;
+}
+
+function clean_unit_of_work_delegate()
+{
+    if_unit_of_work_executed([]);
+    if_unit_of_work_disturbed([]);
+}
+
 function unit_of_work(Closure $action)
 {
     local_cache_delete_all();
+    clean_unit_of_work_delegate();
 
     try {
-        $res = $action();
+        try {
+            $res = $action();
 
-        $entities = local_cache_get_all();
-    } finally {
-        local_cache_delete_all();
-    }
-
-    $sqls = [];
-
-    $db_config_key = unit_of_work_db_config_key();
-
-    foreach ($entities as $entity) {
-
-        $dao = $entity->get_dao();
-
-        if ($dao->get_db_config_key() !== $db_config_key) {
-            continue;
+            $entities = local_cache_get_all();
+        } finally {
+            local_cache_delete_all();
         }
 
-        if ($entity->just_force_deleted()) {
-            if (!$entity->just_new()) {
-                $sqls[] = $dao->dump_delete_sql($entity);
+        $sqls = [];
+
+        $db_config_key = unit_of_work_db_config_key();
+
+        foreach ($entities as $entity) {
+
+            $dao = $entity->get_dao();
+
+            if ($dao->get_db_config_key() !== $db_config_key) {
+                continue;
             }
-        } elseif ($entity->just_new()) {
-            $sqls[] = $dao->dump_insert_sql($entity);
-        } elseif ($entity->just_updated() || $entity->just_deleted()) {
-            $sqls[] = $dao->dump_update_sql($entity);
-        }
-    }
 
-    if ($sqls) {
-        if (count($sqls) > 1) {
-            db_transaction(function () use ($sqls, $db_config_key) {
-                foreach ($sqls as $sql) {
-                    _unit_of_work_write($sql['sql_template'], $sql['binds'], $db_config_key);
+            if ($entity->just_force_deleted()) {
+                if (!$entity->just_new()) {
+                    $sqls[] = $dao->dump_delete_sql($entity);
                 }
-            }, $db_config_key);
-        } else {
-            $sql = reset($sqls);
-            _unit_of_work_write($sql['sql_template'], $sql['binds'], $db_config_key);
+            } elseif ($entity->just_new()) {
+                $sqls[] = $dao->dump_insert_sql($entity);
+            } elseif ($entity->just_updated() || $entity->just_deleted()) {
+                $sqls[] = $dao->dump_update_sql($entity);
+            }
         }
+
+        if ($sqls) {
+            if (count($sqls) > 1) {
+                db_transaction(function () use ($sqls, $db_config_key) {
+                    foreach ($sqls as $sql) {
+                        _unit_of_work_write($sql['sql_template'], $sql['binds'], $db_config_key);
+                    }
+                }, $db_config_key);
+            } else {
+                $sql = reset($sqls);
+                _unit_of_work_write($sql['sql_template'], $sql['binds'], $db_config_key);
+            }
+        }
+
+        $action = if_unit_of_work_executed();
+        if ($action instanceof closure) {
+            call_user_func($action);
+            clean_unit_of_work_delegate();
+        }
+
+    } catch (exception $ex) {
+
+        $action = if_unit_of_work_disturbed();
+        if ($action instanceof closure) {
+            call_user_func($action, $ex);
+            clean_unit_of_work_delegate();
+        }
+
+        throw $ex;
     }
 
     return $res;
