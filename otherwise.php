caa19f5d4dcc799347bece826172d8e3b8eb2014
yaoyang <kiki@smarty.so>
2021-04-07 14:33:13 +0800

    otherwise 的报错码支持字符串

--- otherwise.php
+++ otherwise.php
@@ -1,11 +1,32 @@
 <?php
 
+define('OTHERWISE_MESSAGE_DELIMITER', '---');
+
 class business_exception extends exception { }
 
 function otherwise($assertion, $description = 'assertion is not true', $exception_class_name = 'exception', $exception_code = 'OTHERWISE_DEFAULT')
 {/*{{{*/
     if (! $assertion) {
-        throw new $exception_class_name($description, $exception_code);
+        throw new $exception_class_name($exception_code.OTHERWISE_MESSAGE_DELIMITER.$description, -1);
+    }
+}/*}}}*/
+
+function otherwise_get_error_info(throwable $ex)
+{/*{{{*/
+    $message = $ex->getMessage();
+
+    $info = explode(OTHERWISE_MESSAGE_DELIMITER, $message);
+
+    if (count($info) > 1) {
+        return [
+            'code' => $info[0],
+            'message' => $info[1],
+        ];
+    } else {
+        return [
+            'code' => $ex->getCode(),
+            'message' => $message,
+        ];
     }
 }/*}}}*/
 
@@ -22,7 +43,7 @@ function otherwise_error_code($error_code, $assertion, array $replace_contents =
                 $description = str_replace($replace_key, $content, $description);
             }
         }
-
-        throw new business_exception($description, $error_code);
+        
+        throw new business_exception($error_code.OTHERWISE_MESSAGE_DELIMITER.$description, -1);
     }
 }/*}}}*/
